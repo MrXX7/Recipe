4f2320a487209040988c53093922e9eb7fc54ac2
Oncu <oohancan@gmail.com>
2023-09-05 01:28:58 +0300

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Oncu Can on 5.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
